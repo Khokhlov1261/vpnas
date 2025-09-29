a66e6d42c458112535220a7adc4df5643877b7cd
Khokhlov1261 <147748014+Khokhlov1261@users.noreply.github.com>
2025-09-29 17:20:48 +0500

    4

--- simple_bot.py
+++ simple_bot.py
@@ -237,4 +237,4 @@ async def set_webhook():
 if __name__ == "__main__":
     import asyncio
     asyncio.run(set_webhook())
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 9000)))
